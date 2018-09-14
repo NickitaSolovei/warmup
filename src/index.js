1aa81c1c22715c681985116dd1ecaac984d476c3
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-14 21:18:30 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function warmup(temperature) {
   // your implementation here
+  //из шкалы Цельсия в шкалу Фаренгейта нужно умножить исходное число на 9/5 и прибавить 32. 
+  
+  var tempF = temperature * 9 / 5 + 32;
+  return tempF;
 };
